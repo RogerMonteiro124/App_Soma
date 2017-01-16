3f775d1eb8cf5426b9aca10c8afc2c007de2ddc2
RogerMonteiro124 <rogermonteiro15@outlook.com>
2017-01-16 16:48:57 -0400

    Initial commit

--- app/src/main/java/teste/soma/MainActivity.java
+++ app/src/main/java/teste/soma/MainActivity.java
@@ -1,15 +1,11 @@
 package teste.soma;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Intent;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     EditText txtNum1;
