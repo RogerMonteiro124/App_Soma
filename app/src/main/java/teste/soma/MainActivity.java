6e76bf32207fd6f50322ca5d69c9f0e40d370352
RogerMonteiro124 <rogermonteiro15@outlook.com>
2017-01-17 00:36:15 -0400

    Update

--- app/src/main/java/teste/soma/MainActivity.java
+++ app/src/main/java/teste/soma/MainActivity.java
@@ -3,9 +3,11 @@ package teste.soma;
 import android.app.Activity;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     EditText txtNum1;
@@ -27,80 +29,92 @@ public class MainActivity extends Activity {
         btn4=(Button) findViewById(R.id.btndiv);
         Result.setKeyListener(null);
         btn1.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {Double n1=Double.parseDouble(txtNum1.getText().toString());
-                    Double n2=Double.parseDouble(txtNum2.getText().toString());
+            @Override
+            public void onClick(View v) {
+                Double n1=Double.parseDouble(txtNum1.getText().toString());
+                Double n2=Double.parseDouble(txtNum2.getText().toString());
+                if(txtNum1.getText().length()==0 || txtNum2.getText().length()==0) {
+                    AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
+                    dialogo.setTitle("Alerta:");
+                    dialogo.setMessage("\nSão necessários 2 números!");
+                    dialogo.setNeutralButton("Voltar", null);
+                    dialogo.show();
+                }
                     Double res=n1+n2;
                     String Resultado=new Double(res).toString();
                     Result.setText(Resultado);
                     Result.setKeyListener(null);
-                    //AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
-
-                    /*dialogo.setTitle("Resultado:");
-                    dialogo.setMessage("\nA soma de "+n1+" + "+n2+" é = "+res);
-                    dialogo.setNeutralButton("SAIR",null);
-                    dialogo.show();*/
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Double n1=Double.parseDouble(txtNum1.getText().toString());
+                Double n2=Double.parseDouble(txtNum2.getText().toString());
+                if(txtNum1.getText().length()==0 || txtNum2.getText().length()==0) {
+                    AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
+                    dialogo.setTitle("Alerta:");
+                    dialogo.setMessage("\nSão necessários 2 números!");
+                    dialogo.setNeutralButton("Voltar", null);
+                    dialogo.show();
                 }
-            });
-            btn2.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Double n1=Double.parseDouble(txtNum1.getText().toString());
-                    Double n2=Double.parseDouble(txtNum2.getText().toString());
-                    Double res=n1*n2;
+                    Double res = n1 * n2;
+                    String Resultado = new Double(res).toString();
+                    Result.setText(Resultado);
+                    Result.setKeyListener(null);
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Double n1=Double.parseDouble(txtNum1.getText().toString());
+                Double n2=Double.parseDouble(txtNum2.getText().toString());
+                if(txtNum1.getText().length()==0 || txtNum2.getText().length()==0) {
+                    AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
+                    dialogo.setTitle("Alerta:");
+                    dialogo.setMessage("\nSão necessários 2 números!");
+                    dialogo.setNeutralButton("Voltar", null);
+                    dialogo.show();
+                }
+                    Double res=n1 - n2;
                     String Resultado=new Double(res).toString();
                     Result.setText(Resultado);
                     Result.setKeyListener(null);
-                    /*
-                    AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
 
-                    dialogo.setTitle("Resultado:");
-                    dialogo.setMessage("\nA Multiplicação de "+n1+" * "+n2+" é = "+res);
-                    dialogo.setNeutralButton("SAIR",null);
-                    dialogo.show();*/
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Double n1=Double.parseDouble(txtNum1.getText().toString());
+                Double n2=Double.parseDouble(txtNum2.getText().toString());
+                if(txtNum1.getText().length()==0 || txtNum2.getText().length()==0) {
+                    AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
+                    dialogo.setTitle("Alerta:");
+                    dialogo.setMessage("\nSão necessários 2 números!");
+                    dialogo.setNeutralButton("Voltar", null);
+                    dialogo.show();
+
                 }
-            });
-            btn3.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Double n1=Double.parseDouble(txtNum1.getText().toString());
-                    Double n2=Double.parseDouble(txtNum2.getText().toString());
-                    Double res=n1-n2;
+                if (n2==0){
+                    AlertDialog.Builder error = new AlertDialog.Builder(teste.soma.MainActivity.this);
+                    error.setTitle("Erro");
+                    error.setMessage("\nImpossivel Divisão por zero!\n");
+                    error.show();
+                    Result.setText("");
+                    Result.setKeyListener(null);
+                }
+                else{
+                    Double res=n1 / n2;
                     String Resultado=new Double(res).toString();
                     Result.setText(Resultado);
                     Result.setKeyListener(null);
-                    /*
-                    AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
-
-                    dialogo.setTitle("Resultado:");
-                    dialogo.setMessage("\nA Subtração de "+n1+" - "+n2+" é = "+res);
-                    dialogo.setNeutralButton("SAIR",null);
-                    dialogo.show();*/
                 }
-            });
-            btn4.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Double n1=Double.parseDouble(txtNum1.getText().toString());
-                    Double n2=Double.parseDouble(txtNum2.getText().toString());
-                    if (n2==0){
-                        AlertDialog.Builder error = new AlertDialog.Builder(teste.soma.MainActivity.this);
-                        error.setTitle("Erro");
-                        error.setMessage("\nImpossivel Divisão por Zero\n");
-                        error.show();
-                    }else {
-                        Double res = n1 / n2;
-                        String Resultado=new Double(res).toString();
-                        Result.setText(Resultado);
-                        Result.setKeyListener(null);
-                        /*AlertDialog.Builder dialogo = new AlertDialog.Builder(teste.soma.MainActivity.this);
-
-                        dialogo.setTitle("Resultado:");
-                        dialogo.setMessage("\nA Divisão de " + n1 + " / " + n2 + " é = " + res);
-                        dialogo.setNeutralButton("SAIR", null);
-                        dialogo.show();*/
-                    }
-                }
-            });
-        }
+            }
+        });
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("http://www.github.com/RogerMonteiro124");
+        Linkify.addLinks(textView, Linkify.WEB_URLS);
     }
+}
